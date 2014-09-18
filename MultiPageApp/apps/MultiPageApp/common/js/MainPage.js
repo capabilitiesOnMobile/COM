f0a7c17d68cffaa7cbcffc471a1db198ad180b6e
Carlos Henrique Dalla Stella <carloshenrique@carloshenrique.local>
2014-09-18 15:01:35 +0200

    Corrected buttons

--- MultiPageApp/apps/MultiPageApp/common/js/MainPage.js
+++ MultiPageApp/apps/MultiPageApp/common/js/MainPage.js
@@ -1,52 +1,18 @@
-/*
- *
-    COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
-    these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
-    application programs conforming to the application programming interface for the operating platform for which the sample code is written.
-    Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
-    EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
-    FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
-    INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
-    IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+/* Script MainPage.js */
 
- */
+mainPage = {};
 
-currentPage = {};
-
-currentPage.init = function(){
+mainPage.init = function(){
 	WL.Logger.debug("MainPage :: init");
 };
 
-currentPage.loadPage = function(directory,script){
-	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + directory);
-	pagesHistory.push(path + "pages/MainPage.html");
-	$("#pagePort").load(path + "pages/" + directory + ".html");
-	$.getScript(path + "js/" + script + ".js");/*, function() {
-		if (currentPage.init) {
-			currentPage.init();
-		}
-	});*/
-};
-
-
-currentPage.loadPagePort = function(previousFileDirectory, NewFileDirectory,script){
-	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + NewFileDirectory);
-	pagesHistory.push(path + "pages/" + previousFileDirectory + ".html");
-	$("#pagePort").load(path + "pages/" + NewFileDirectory + ".html");
-	$.getScript(path + "js/" + script + ".js");/*, function() {
-	if (currentPage.init) {
-	currentPage.init();
-}
-});*/
-};
-
-currentPage.loadBandeau = function(previousFileDirectory, NewFileDirectory,script){
-	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + previousFileDirectory);
-	pagesHistory.push(path + "pages/" + previousFileDirectory + ".html");
-	$("#bandeau").load(path + "pages/" + NewFileDirectory + ".html");
-	$.getScript(path + "js/" + script + ".js");/*, function() {
-			if (currentPage.init) {
-			currentPage.init();
-		}
-		});*/
+mainPage.loadPage = function(previousDirectory, newDirectory, script){
+	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + newDirectory);
+	historyBandeau.push(path + "pages/" + previousDirectory + "Bandeau.html");
+	historyPagePort.push(path + "pages/" + previousDirectory + ".html");
+	historyLogo.push(path + "pages/" + "Logo" + ".html");
+	$("#bandeau").load(path + "pages/" + newDirectory + "Bandeau.html");
+	$("#pagePort").load(path + "pages/" + newDirectory + ".html");
+	$("#logo").load(path + "pages/" + "Logo" + ".html");
+	$.getScript(path + "js/" + script + ".js");
 };
